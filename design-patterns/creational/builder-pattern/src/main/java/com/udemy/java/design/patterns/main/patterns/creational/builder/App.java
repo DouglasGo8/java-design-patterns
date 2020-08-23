ad12155ceafead79dc01549d50297194624c4a1c
DouglasGo8 <sgtear@hotmail.com>
2020-08-23 18:55:27 -0300

    builder artifact

---
+++ design-patterns/creational/builder-pattern/src/main/java/com/udemy/java/design/patterns/main/patterns/creational/builder/App.java
@@ -0,0 +1,11 @@
+
+package com.udemy.java.design.patterns.main.patterns.creational.builder;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ design-patterns/creational/builder-pattern/src/main/java/com/udemy/java/design/patterns/main/patterns/creational/builder/User.java
@@ -0,0 +1,14 @@
+
+
+package com.udemy.java.design.patterns.main.patterns.creational.builder;
+
+public class User {
+
+
+    private String lastName;
+    private String firstName;
+
+
+    
+    
+}
